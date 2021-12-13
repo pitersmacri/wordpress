4ba27475ef76b0d6b7a1e5e1c24629ca0aaf96d5
Piters Macri <piters@qualitare.com.br>
2021-12-13 18:33:33 -0300

    adicionando comentários em português

--- page_id_query_fixed.php
+++ page_id_query_fixed.php
@@ -1,14 +1,14 @@
-<!--  querie post type page with ID 12 -->
+<!--  query post type page with ID 12 - querie do tipo page com ID 12 -->
 <?php $my_query = new WP_Query(array('p' => 12,'post_type' => 'page')); ?>
 <?php if ( $my_query->have_posts() ) : ?>
 <?php while ( $my_query->have_posts() ) : ?>
 <?php $my_query->the_post(); ?>
 
-	<!-- print content page ID 12 -->
+	<!-- print content page ID 12 - exibe conteúdo da página ID 12 -->
 	<?php the_content(); ?>
 						
 <?php endwhile;	?>
-<!--  reset querie-->
+<!--  reset query - reseta a querie -->
 <?php wp_reset_postdata(); ?>
 <?php endif; ?>
-<!-- end loop -->
+<!-- end loop - fim do laço -->
